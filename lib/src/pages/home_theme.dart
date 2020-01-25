c7c2f62fd3ee23355386bb0644bf9d274474d912
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-25 18:40:52 -0500

    trailing and leading

--- lib/src/pages/home_theme.dart
+++ lib/src/pages/home_theme.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Kerugui', 'Poomsae', 'TK5', 'Rutinas', 'Ranking'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: (AppBar(title: Text("Componentes APP"))),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text("Combate Inicial"),
-          ),
-          // Coloca una raya como un hr
-          // Divider(),
-          Divider(),
-          ListTile(
-            title: Text("Combate Inicial"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("Combate Inicial"),
-          )
-        ],
+        // children: _crearItems(),
+        children: _crearItemsCortos(),
       ),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(Divider(
+          color: Colors.black,
+        ));
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCortos() {
+    return opciones
+        .map((item) => Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(item),
+                  subtitle: Text("Sistema Deportivo"),
+                  //Leading coloca items delante del contenido mientras que trailing al final
+                  leading: Icon(Icons.account_circle),
+                  trailing: Icon(Icons.arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  color: Colors.black,
+                )
+              ],
+            ))
+        .toList();
   }
 }
