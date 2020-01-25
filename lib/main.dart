a1014d177b242b3c46d958038a2c1128572ff028
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-25 15:55:27 -0500

    flutter app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:components/src/pages/home_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Componentes App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Componentes'),
+          ),
+          body: HomePageTemp()),
+    );
+  }
+}
