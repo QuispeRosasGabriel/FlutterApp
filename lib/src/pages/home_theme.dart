71e04d45ad53c68d68dbaea42441f6396bf54fd3
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-25 16:46:20 -0500

    working with divider

--- lib/src/pages/home_theme.dart
+++ lib/src/pages/home_theme.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home gaaaa"));
+    return Scaffold(
+      appBar: (AppBar(title: Text("Componentes APP"))),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Combate Inicial"),
+          ),
+          // Coloca una raya como un hr
+          // Divider(),
+          Divider(),
+          ListTile(
+            title: Text("Combate Inicial"),
+          ),
+          Divider(),
+          ListTile(
+            title: Text("Combate Inicial"),
+          )
+        ],
+      ),
+    );
   }
 }
