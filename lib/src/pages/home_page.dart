5d253e147960d461e41fa67ed324b26bfc127844
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-01-25 19:46:03 -0500

    working new homepage

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Modalidades"),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _listarItems(),
+    );
+  }
+
+  List<Widget> _listarItems() {
+    return [
+      ListTile(title: Text("Kerugi")),
+      Divider(
+        color: Colors.blue,
+      ),
+      ListTile(title: Text("Poomsae")),
+      Divider(
+        color: Colors.red,
+      ),
+      ListTile(title: Text("TK5")),
+      Divider(
+        color: Colors.blue,
+      ),
+      ListTile(title: Text("Rutinas")),
+      Divider(
+        color: Colors.red,
+      ),
+      ListTile(title: Text("Ranking")),
+    ];
+  }
+}
